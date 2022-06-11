06726cf109c29d9eb4203838b31fcae46c747f52
darkopet <darkodepetrovski@gmail.com>
2022-06-11 21:31:44 +0200

    Initial.

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+    function randomString($n) {
+        $characters = '0123456789abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $str = '';
+        for ($i=0; $i < $n; $i++) {
+        $index = rand(0, strlen($characters)-1);
+        $str .= $characters[$index];
+        }
+        return $str;
+    }

90b2e3726c7c346e418df603c37d4a8cc532736b
darkopet <darkodepetrovski@gmail.com>
2022-06-27 15:38:29 +0200

    Server and DB init

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php 
 
     # DSN string = defines the connection string of the database
-    $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'phpmyadmin', 'phpmyadmindb+--+');
+    $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'phpmyadmin', 'phpmyadmindb00+--+');
     # If the connection to the database is not succesfull:
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- functions.php
+++ functions.php
@@ -1,11 +1,12 @@
 <?php
-
     function randomString($n) {
         $characters = '0123456789abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
         $str = '';
+
         for ($i=0; $i < $n; $i++) {
-        $index = rand(0, strlen($characters)-1);
-        $str .= $characters[$index];
+            $index = rand(0, strlen($characters)-1);
+            $str .= $characters[$index];
         }
+        
         return $str;
     }
